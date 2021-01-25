cc5b351255eaf83866073b417b31b0f9ac377a8b
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2021-01-25 16:11:01 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Name Sahassawawt Udomsuktham ID 6181267
+
+import Vue from 'vue'
+import App from './App.vue'
+
+import '@/assets/todo.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
